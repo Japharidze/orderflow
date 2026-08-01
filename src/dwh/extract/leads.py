78c304ae525b90931ae9315e4dac8621c8e2232c
Sergi Japharidze <sergi.japharidze@gmail.com>
2026-08-01 19:35:53 +0400

    extract leads add

--- src/dwh/extract/leads.py
+++ src/dwh/extract/leads.py
@@ -6,5 +6,6 @@ from dwh.config import LEADS_FILE
 
 
 def extract_leads() -> Iterator[list[dict]]:
-    df = pd.read_excel(LEADS_FILE)
+    df = pd.read_excel(LEADS_FILE, dtype=str)
+    df = df.where(df["cuit"].notna(), None) # convert NaN to None for cuit, since it's optional
     yield df.to_dict(orient="records") # yield a single batch of all leads, since the file is small
